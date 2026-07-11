ed3bd1c0610bee4fc0f653cb21bcb6f2a33b1ff0
kaweesha weerasinghe <weerasinghamkh.23@uom.lk>
2026-07-11 21:55:01 +0530

    Integrate add admin process control

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -17,3 +17,41 @@ exports.getRegistrationStats = async (req, res) => {
     res.status(500).json({ message: 'Server error during fetching registration stats' });
   }
 };
+
+const bcrypt = require('bcryptjs'); 
+
+exports.createAdmin = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+    // Check if the user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { email }
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists with this email' });
+    }
+// Hash the password before saving
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newAdmin = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role: 'admin'
+      }
+    });
+
+    res.status(201).json({
+      message: 'New Administrator created successfully',
+      email: newAdmin.email
+    });
+
+  } catch (error) {
+    console.error('Create Admin Error:', error);
+    res.status(500).json({ message: 'Server error during creating admin' });
+  }
+};
