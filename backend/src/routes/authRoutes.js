2840310249768b74cc0ec86e2e79cf6d3d404cd6
kaweesha weerasinghe <weerasinghamkh.23@uom.lk>
2026-07-11 22:28:07 +0530

    Update auth routes for update pw

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // 1. SIGNUP Route (POST /api/auth/signup)
 router.post('/signup', authController.register);
 
 // 2. LOGIN Route (POST /api/auth/login)
 router.post('/login', authController.login);
+
+// POST /api/auth/change-password
+router.post('/change-password', authMiddleware, authController.changePassword);
 
 module.exports = router;
