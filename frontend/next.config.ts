fd5c9f59eb4d95f8bef20069666162e4116c666a
kaweesha weerasinghe <weerasinghamkh.23@uom.lk>
2026-07-13 16:22:08 +0530

    Remove dev indicator

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   devIndicators: false,
+  devIndicators: {
+    appIsrStatus: false,
+    buildActivity: false,
+  },
 };
 
 export default nextConfig;
